e3a7800bfc8ab2b8957fa02d2adf51a566deba60
taimv2405 <taimv2405@gmail.com>
2026-07-06 09:11:16 +0000

    [19] fix: remove exposed ports from all services except nginx

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080/api/',
 })
 
 export default apiClient
